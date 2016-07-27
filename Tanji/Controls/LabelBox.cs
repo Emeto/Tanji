1e2c10bd7a3a88ae14d0d52e403e52b6439d522f
ArachisH <TheOldNutMan@gmail.com>
2016-07-26 19:09:25 -0700

    Added a cool new TextBox(LabelBox).

--- Tanji/Controls/LabelBox.cs
+++ Tanji/Controls/LabelBox.cs
@@ -5,13 +5,21 @@ namespace Tanji.Controls
 {
     public class LabelBox : TextBox
     {
-        public static readonly DependencyProperty TitleProperty =
-            DependencyProperty.Register(nameof(Title), typeof(string), typeof(LabelBox), null);
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title),
+            typeof(string), typeof(LabelBox), new UIPropertyMetadata("Title"));
+
+        public static readonly DependencyProperty RelativeWidthProperty = DependencyProperty.Register(nameof(RelativeWidth),
+            typeof(double), typeof(LabelBox), new UIPropertyMetadata(100.0));
 
         public string Title
         {
             get { return GetValue(TitleProperty) as string; }
             set { SetValue(TitleProperty, value); }
+        }
+        public double RelativeWidth
+        {
+            get { return (double)GetValue(RelativeWidthProperty); }
+            set { SetValue(RelativeWidthProperty, value); }
         }
     }
 }
